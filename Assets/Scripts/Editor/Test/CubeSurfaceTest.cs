85c5cda386545f02538e1b10e78ac27994a7d968
wlindley <walker@walkerlindley.com>
2015-08-11 14:17:00 -0700

    Implemented CubeSurface and renamed Test.unity to PlaneTest.unity

---
+++ Assets/Scripts/Editor/Test/CubeSurfaceTest.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using NUnit.Framework;
+using NSubstitute;
+using CamCon;
+
+namespace CamConTest
+{
+    [TestFixture]
+    public class CubeSurfaceTest
+    {
+        private CubeSurface testObj;
+        private Vector3 origin = new Vector3(0, 0, 0);
+        private float sideLength = 10f;
+        private Vector3 up = new Vector3(0, 1, 0);
+
+        [SetUp]
+        public void SetUp()
+        {
+            testObj = new CubeSurface(origin, sideLength, up);
+        }
+
+        [Test]
+        public void GetOriginReturnsSpecifiedOrigin()
+        {
+            Assert.AreEqual(origin, testObj.GetOrigin());
+        }
+
+        [Test]
+        public void GetWorldUpVectorReturnsSpecifiedUpVector()
+        {
+            Assert.AreEqual(up, testObj.GetWorldUpVector());
+        }
+
+        [Test]
+        public void GetNormalAtPointReturnsPositiveXUnitVectorWhenPointIsOnRightSide()
+        {
+            Assert.AreEqual(Vector3.right, testObj.GetNormalAtPoint(new Vector3(sideLength * .5f, 0f, 0f)));
+        }
+
+        [Test]
+        public void GetNormalAtPointReturnsNegativeZUnitVectorWhenPointIsOnBackSide()
+        {
+            Assert.AreEqual(-Vector3.forward, testObj.GetNormalAtPoint(new Vector3(0f, 0f, -sideLength * .5f)));
+        }
+
+        [Test]
+        public void GetNormalAtPointReturnsPositiveYUnitVectorWhenPointIsOnTopSide()
+        {
+            Assert.AreEqual(Vector3.up, testObj.GetNormalAtPoint(new Vector3(0f, sideLength * .5f, 0f)));
+        }
+    }
+}
---
+++ Assets/Scripts/Runtime/CubeSurface.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+
+namespace CamCon
+{
+    public class CubeSurface : Surface
+    {
+        private Vector3 origin;
+        private Vector3 up;
+        private float sideLength;
+
+        public CubeSurface(Vector3 origin, float sideLength, Vector3 up)
+        {
+            this.origin = origin;
+            this.sideLength = sideLength;
+            this.up = up;
+        }
+        public Vector3 GetOrigin()
+        {
+            return origin;
+        }
+
+        public Vector3 GetWorldUpVector()
+        {
+            return up;
+        }
+
+        public Vector3 GetNormalAtPoint(Vector3 position)
+        {
+            var delta = position - origin;
+            if (Mathf.Abs(delta.x) >= sideLength * .5f)
+                return new Vector3(delta.x / Mathf.Abs(delta.x), 0f, 0f);
+            else if (Mathf.Abs(delta.y) >= sideLength * .5f)
+                return new Vector3(0f, delta.y / Mathf.Abs(delta.y), 0f);
+            else if (Mathf.Abs(delta.z) >= sideLength * .5f)
+                return new Vector3(0f, 0f, delta.z / Mathf.Abs(delta.z));
+            return Vector3.zero;
+        }
+    }
+}
