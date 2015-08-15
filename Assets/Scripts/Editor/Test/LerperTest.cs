9b704f4a958f5cb80f5719f8b90c9ee51081eb18
wlindley <walker@walkerlindley.com>
2015-08-15 11:57:28 -0700

    Implemented a simple linear interpolation algorithm

---
+++ Assets/Scripts/Editor/Test/LerperTest.cs
@@ -0,0 +1,77 @@
+﻿using UnityEngine;
+using NUnit.Framework;
+using NSubstitute;
+using CamCon;
+
+namespace CamConTest
+{
+    [TestFixture]
+    public class LerperTest
+    {
+        private Lerper testObj;
+
+        [Test]
+        [ExpectedException(ExpectedException=typeof(LerperException), ExpectedMessage="Lerper has no data")]
+        public void ProbeThrowsExceptionWhenNoKeyPointsAreDefined()
+        {
+            testObj = new Lerper();
+            testObj.Probe(.5f);
+        }
+
+        [Test]
+        public void ProbeAlwaysReturnsDefinedValueWhenOnlyOneKeyPointWasDefined()
+        {
+            var expectedValue = 5f;
+            testObj = new Lerper(new LerperPair(.5f, expectedValue));
+            AssertInputMapsToOutput(0f, expectedValue);
+            AssertInputMapsToOutput(.5f, expectedValue);
+            AssertInputMapsToOutput(1f, expectedValue);
+        }
+
+        [Test]
+        public void ProbeReturnsLinearlyInterpolatedValuesWhenTwoKeyPointsAreDefined()
+        {
+            testObj = new Lerper(new LerperPair(1f, 5f), new LerperPair(3f, 6f));
+            AssertInputMapsToOutput(1f, 5f);
+            AssertInputMapsToOutput(1.5f, 5.25f);
+            AssertInputMapsToOutput(2f, 5.5f);
+            AssertInputMapsToOutput(2.5f, 5.75f);
+            AssertInputMapsToOutput(3f, 6f);
+        }
+
+        [Test]
+        public void ProbeReturnsFirstOutputValueWhenInputIsBelowMinimum()
+        {
+            testObj = new Lerper(new LerperPair(10f, 5f), new LerperPair(20f, 10f));
+            AssertInputMapsToOutput(1f, 5f);
+        }
+
+        [Test]
+        public void ProbeReturnsLastOutputValueWhenInputIsAboveMaximum()
+        {
+            testObj = new Lerper(new LerperPair(10f, 5f), new LerperPair(20f, 10f));
+            AssertInputMapsToOutput(25f, 10f);
+        }
+
+        [Test]
+        public void ProbeReturnsLinearlyInterpolatedValuesWhenThreeKeyPointsAreDefined()
+        {
+            testObj = new Lerper(new LerperPair(1f, 5f), new LerperPair(3f, 6f), new LerperPair(5f, 8f));
+            AssertInputMapsToOutput(1f, 5f);
+            AssertInputMapsToOutput(2f, 5.5f);
+            AssertInputMapsToOutput(3f, 6f);
+            AssertInputMapsToOutput(4f, 7f);
+            AssertInputMapsToOutput(5f, 8f);
+        }
+
+        private void AssertInputMapsToOutput(float input, float output)
+        {
+            AssertAreClose(output, testObj.Probe(input));
+        }
+
+        private void AssertAreClose(float expected, float actual)
+        {
+            Assert.AreEqual(expected, actual, float.Epsilon);
+        }
+    }
+}
---
+++ Assets/Scripts/Runtime/Lerper.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace CamCon
+{
+    public class Lerper
+    {
+        private LerperPair[] data;
+
+        public Lerper(params LerperPair[] dataPoints)
+        {
+            this.data = dataPoints;
+        }
+
+        virtual public float Probe(float input)
+        {
+            if (0 == data.Length)
+                throw new LerperException("Lerper has no data");
+            else if (1 == data.Length)
+                return data[0].Output;
+
+            return GetOutputForInput(input);
+        }
+
+        private float GetOutputForInput(float input)
+        {
+            var min = data[0];
+            var max = data[1];
+
+            for (var i = 1; i < data.Length && max.Input < input; i++)
+            {
+                min = max;
+                max = data[i];
+            }
+
+            var percent = MapInputToRange(input, ref min, ref max);
+            return GetOutputFromRange(percent, ref min, ref max);
+        }
+
+        private float MapInputToRange(float input, ref LerperPair min, ref LerperPair max)
+        {
+            var percent = (input - min.Input) / (max.Input - min.Input);
+            return Mathf.Clamp(percent, 0f, 1f);
+        }
+
+        private float GetOutputFromRange(float percent, ref LerperPair min, ref LerperPair max)
+        {
+            return min.Output + (percent * (max.Output - min.Output));
+        }
+    }
+
+    public struct LerperPair
+    {
+        private float input, output;
+
+        public LerperPair(float input, float output)
+        {
+            this.input = input;
+            this.output = output;
+        }
+
+        public float Input { get { return input; } }
+        public float Output { get { return output; } }
+
+        public override string ToString()
+        {
+            return String.Format("LerperPair: {0} -> {1}", input, output);
+        }
+    }
+
+    public class LerperException : Exception
+    {
+        public LerperException(string message) : base(message) { }
+    }
+}
