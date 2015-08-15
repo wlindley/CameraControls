d515c9a42e0b7b1316eb0c9135a1b6751a790bcc
wlindley <walker@walkerlindley.com>
2015-08-14 22:30:41 -0700

    Implemented SphereSurface

---
+++ Assets/Scripts/Editor/Test/SphereSurfaceTest.cs
@@ -0,0 +1,80 @@
+﻿using UnityEngine;
+using NUnit.Framework;
+using NSubstitute;
+using CamCon;
+
+namespace CamConTest
+{
+    [TestFixture]
+    public class SphereSurfaceTest
+    {
+        private SphereSurface testObj;
+        private Vector3 origin;
+        private float radius;
+        private Vector3 up;
+
+        [SetUp]
+        public void SetUp()
+        {
+            origin = new Vector3(0, 10, 0);
+            radius = 5f;
+            up = new Vector3(0, 1, 0);
+            testObj = new SphereSurface(origin, radius, up);
+        }
+
+        [Test]
+        public void GetInitialPositionOnSurfaceReturnsArbitraryPointOnSurface()
+        {
+            var diff = testObj.GetInitialPointOnSurface() - origin;
+            Assert.AreEqual(radius, diff.magnitude);
+        }
+
+        [Test]
+        public void GetWorldUpVectorReturnsSpecifiedUpVector()
+        {
+            Assert.AreEqual(up, testObj.GetWorldUpVector());
+        }
+
+        [Test]
+        public void GetNormalAtPointReturnsUnitVectorPointingAtPoint(
+            [NUnit.Framework.Random(0.0, 2.0 * Mathf.PI, 2)] double theta,
+            [NUnit.Framework.Random(0.0, 2.0 * Mathf.PI, 2)] double row,
+            [NUnit.Framework.Values(5.0)] double radius)
+        {
+            var pos = origin + (Quaternion.Euler((float)theta, 0f, (float)row) * new Vector3((float)radius, 0f, 0f));
+            Assert.AreEqual((float)radius, (pos - origin).magnitude);
+
+            var normal = testObj.GetNormalAtPoint(pos);
+            Assert.IsTrue(Mathf.Approximately(1f, normal.magnitude));
+            Assert.AreEqual((pos - origin).normalized, normal);
+        }
+
+        [Test]
+        public void ClampPointToSurfaceReturnsPointOnSurfaceAlongNormalWhenPointIsOutsideSphere(
+            [NUnit.Framework.Random(0.0, 2.0 * Mathf.PI, 2)] double theta,
+            [NUnit.Framework.Random(0.0, 2.0 * Mathf.PI, 2)] double row,
+            [NUnit.Framework.Values(5.5, 10f)] double radiusToPoint)
+        {
+            var pos = origin + (Quaternion.Euler((float)theta, 0f, (float)row) * new Vector3((float)radiusToPoint, 0f, 0f));
+            Assert.IsTrue(Mathf.Approximately((float)radiusToPoint, (pos - origin).magnitude));
+
+            var surfacePoint = testObj.ClampPointToSurface(pos);
+            Assert.IsTrue(Mathf.Approximately((float)radius, (surfacePoint - origin).magnitude));
+            Assert.AreEqual(origin + ((pos - origin).normalized * (float)radius), surfacePoint);
+        }
+
+        [Test]
+        public void ClampPointToSurfaceReturnsPointOnSurfaceAlongNormalWhenPointIsInsideSphere(
+            [NUnit.Framework.Random(0.0, 2.0 * Mathf.PI, 2)] double theta,
+            [NUnit.Framework.Random(0.0, 2.0 * Mathf.PI, 2)] double row,
+            [NUnit.Framework.Values(4.5, 1f)] double radiusToPoint)
+        {
+            var pos = origin + (Quaternion.Euler((float)theta, 0f, (float)row) * new Vector3((float)radiusToPoint, 0f, 0f));
+            Assert.IsTrue(Mathf.Approximately((float)radiusToPoint, (pos - origin).magnitude));
+
+            var surfacePoint = testObj.ClampPointToSurface(pos);
+            Assert.IsTrue(Mathf.Approximately((float)radius, (surfacePoint - origin).magnitude));
+            Assert.AreEqual(origin + ((pos - origin).normalized * (float)radius), surfacePoint);
+        }
+    }
+}
---
+++ Assets/Scripts/Runtime/SphereSurface.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System;
+
+namespace CamCon
+{
+    public class SphereSurface : Surface
+    {
+        public static SphereSurface TestInstance;
+        public static SphereSurface GetInstance(Vector3 origin, float radius, Vector3 up)
+        {
+            if (null != TestInstance)
+                return TestInstance;
+            return new SphereSurface(origin, radius, up);
+        }
+
+        private Vector3 origin;
+        private float radius;
+        private Vector3 up;
+
+        public SphereSurface(Vector3 origin, float radius, Vector3 up)
+        {
+            this.origin = origin;
+            this.radius = radius;
+            this.up = up;
+        }
+
+        public Vector3 GetInitialPointOnSurface()
+        {
+            return origin + (radius * Vector3.forward);
+        }
+
+        public Vector3 GetWorldUpVector()
+        {
+            return up;
+        }
+
+        public Vector3 GetNormalAtPoint(Vector3 position)
+        {
+            return (position - origin).normalized;
+        }
+
+        public Vector3 ClampPointToSurface(Vector3 position)
+        {
+            return origin + (GetNormalAtPoint(position) * radius);
+        }
+    }
+}
